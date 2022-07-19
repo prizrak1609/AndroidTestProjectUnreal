24da69f96dcc74e94cbc8813cb24fa25b236d588
Dmytro <gubatenkod@gmail.com>
2022-07-19 20:16:42 +0300

    Initial source commit

---
+++ Source/AndroidTestProject/AndroidTestProject.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AndroidTestProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AndroidTestProject, "AndroidTestProject" );
+
+DEFINE_LOG_CATEGORY(LogAndroidTestProject)
+ 
